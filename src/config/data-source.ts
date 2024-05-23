423cafe2f0551babff3ffcd0f2f36e0790c8829d
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-22 21:59:15 -0300

    chore: add config folder with data base connection

---
+++ src/config/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { EnvObject } from "@/config";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: EnvObject.dbUser,
+  password: EnvObject.dbPassword,
+  database: EnvObject.dbName,
+  synchronize: true,
+  logging: false,
+  entities: [],
+  migrations: [],
+  subscribers: [],
+});
