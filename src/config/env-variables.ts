38f5b3a8690cea74a056f0ba1bb90fd759eddef2
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-23 19:05:02 -0300

    chore: add database host on env variables

--- src/config/env-variables.ts
+++ src/config/env-variables.ts
@@ -1,10 +1,12 @@
 type EnvObjectType = {
+  dbHost: string;
   dbName: string;
   dbUser: string;
   dbPassword: string;
 };
 
 export const EnvObject: EnvObjectType = {
+  dbHost: process.env.DATABASE_HOST || "localhost",
   dbName: process.env.POSTGRES_DB || "postgres",
   dbUser: process.env.POSTGRES_USER || "postgres",
   dbPassword: process.env.POSTGRES_PASSWORD || "postgres",
