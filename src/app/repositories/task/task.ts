fb9d08bd8014dfd4e9c4ff83ec69121e73e29736
Fernando Schneider <93660206+fernando-sch@users.noreply.github.com>
2024-05-31 18:13:25 -0300

    Add base repository class (#6)

---
+++ src/app/repositories/task/task.ts
@@ -0,0 +1,22 @@
+import { InsertResult, Repository } from "typeorm";
+import { BaseRepository } from "@/app/repositories/base";
+import { Task } from "@/app/entities/task";
+
+type CreateParams = {
+  title: string;
+  description: string | null;
+};
+
+export class TaskRepository extends BaseRepository<Task> {
+  private repository: Repository<Task> = this.getRepository();
+
+  constructor() {
+    super(new Task());
+  }
+
+  create(params: CreateParams): Promise<InsertResult> {
+    const taskEntity = this.repository.manager.create(Task, params);
+
+    return this.repository.insert(taskEntity);
+  }
+}
