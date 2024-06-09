0fa393912e403693480c72e2f278de625882048a
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-09 14:15:21 -0300

    chore: start using data manager class on app

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import * as express from "express";
-import { AppDataSource } from "@/config";
-import { routes } from "@/app_web/routes";
+import { DataSourceOptions } from "typeorm";
+import { EnvObject } from "@/config";
+import app, { connectToDb } from "@/app";
 
-const app = express();
+const dataSourceOptions: DataSourceOptions = {
+  type: "postgres",
+  host: EnvObject.dbHost,
+  port: EnvObject.dbPort,
+  username: EnvObject.dbUser,
+  password: EnvObject.dbPassword,
+  database: EnvObject.dbName,
+  synchronize: true,
+  logging: false,
+};
 
-app.use(express.json());
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization:", err);
-  });
-
-app.use("/", routes);
+connectToDb(dataSourceOptions);
 
 app.listen(8080, () => "Server running on port 8080");
